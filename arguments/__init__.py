cb6120aa642dad7b25b579cc3e53960979d1e189
yeliu <yeliu@deepmirror.com>
2023-10-25 17:04:38 +0800

    fix

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -88,7 +88,7 @@ class OptimizationParams(ParamGroup):
         self.lambda_dssim = 0.2
         self.densification_interval = 200
         self.opacity_reset_interval = 3000
-        self.densify_from_iter = 100
+        self.densify_from_iter = 500
         self.densify_until_iter = 15_000
         self.densify_grad_threshold = 0.0002
         self.random_background = False
